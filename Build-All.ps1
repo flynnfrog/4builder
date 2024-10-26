2b20dc8b02baabc8ef226ed746ecf7402831856e
flynnfrog <flynnfrog>
2024-10-26 15:17:11 +0200

    feat: probable support

--- Build-All.ps1
+++ Build-All.ps1
@@ -1,4 +1,4 @@
-$projects = @("4cade", "4live", "4sports", "a2fc", "adventure-replay", "anti-m", "dhrslide", "million-perfect-letters", "million-perfect-tiles", "passport", "pitch-dark", "pitch-darker", "untitled-word-game", "untitled-word-game-pro", "wizard-replay", "word-clock")
+$projects = @("4cade", "4live", "4sports", "a2fc", "adventure-replay", "anti-m", "dhrslide", "million-perfect-letters", "million-perfect-tiles", "passport", "pitch-dark", "pitch-darker", "probable", "untitled-word-game", "untitled-word-game-pro", "wizard-replay", "word-clock")
 
 foreach ($project in $projects) {
     & podman build --build-arg-file "./etc/$project.env" -t "$project" .
