54a88243c796765ecd182978654ad3dc26b17910
flynnfrog <flynnfrog>
2024-11-02 21:40:39 +0100

    fix: no more list update required for build all scripts

--- Build-All.ps1
+++ Build-All.ps1
@@ -1,9 +1,10 @@
-$projects = @("4cade", "4live", "4sports", "a2fc", "adventure-replay", "anti-m", "dhrslide", "million-perfect-letters", "million-perfect-tiles", "passport", "pitch-dark", "pitch-darker", "probable", "untitled-word-game", "untitled-word-game-pro", "wizard-replay", "word-clock")
+#Requires -Version 7
 
 podman build -f builder.Dockerfile -t 4builder .
-foreach ($project in $projects) {
-    & podman build -f final.Dockerfile --build-arg-file ./etc/$project.env -t $project .
+Get-ChildItem -File -Path 'etc' -Filter '*.env' | Select-Object -Property BaseName -ExpandProperty BaseName | ForEach-Object {
+    $Project=$_
+    & podman build -f final.Dockerfile --build-arg-file ./etc/$Project.env -t $Project .
     & podman image prune -f --filter label=stage=4builder
-    & podman run --rm --mount "src=$(Get-Location),target=/mnt/output,type=bind" "$project"
+    & podman run --rm --mount "src=$(Get-Location),target=/mnt/output,type=bind" "$Project"
     & podman image prune -a -f --filter label=stage=4final
 }
--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-projects="4cade 4live 4sports a2fc adventure-replay anti-m dhrslide million-perfect-letters million-perfect-tiles passport pitch-dark pitch-darker probable untitled-word-game untitled-word-game-pro wizard-replay word-clock"
-
 podman build -f builder.Dockerfile -t 4builder .
-for project in $projects; do
-    podman build -f final.Dockerfile --build-arg-file ./etc/$project.env -t $project .
-    podman image prune -f --filter label=stage=4builder
-    podman run --rm --mount "type=bind,src=$(pwd),target=/mnt/output" "$project"
-    podman image prune -a -f --filter label=stage=4final
+for file in etc/*.env; do 
+    if [ -f "$file" ]; then 
+        project=$(basename "$file" .env) 
+        podman build -f final.Dockerfile --build-arg-file ./etc/$project.env -t $project .
+        podman image prune -f --filter label=stage=4builder
+        podman run --rm --mount "type=bind,src=$(pwd),target=/mnt/output" "$project"
+        podman image prune -a -f --filter label=stage=4final
+    fi 
 done
